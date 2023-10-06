03a0ed62828c43401f595bf911cdc9602a3d2944
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-06 17:11:19 -0400

    add filteredModels in compute, display different device names for dfiferent device type

--- src/components/Support.vue
+++ src/components/Support.vue
@@ -5,7 +5,14 @@
     <v-select
       label="Device type"
       :items="deviceTypes"
+      v-model="selectedDeviceType"
       variant="outlined"
+    ></v-select>
+    <v-select
+      label="Model"
+      :items="filteredModels"
+      variant="outlined"
+      disabled="!selectedDeviceType"
     ></v-select>
   </div>
 </template>
@@ -18,12 +25,31 @@ import {
   getiPhoneModels,
   getMacModels, 
 } from '@/utils/getDeviceModelName'
+import * as constants from '@/constants';
 export default {
   name: 'SupportPage',
   data() {
     return {
-      deviceTypes: getDeviceTypeList()
+      deviceTypes: getDeviceTypeList(),
+      selectedDeviceType: null,
     }
+  },
+  computed: {
+    filteredModels() {
+      // Determine which model list to use based on the selected device type
+      switch (this.selectedDeviceType) {
+        case constants.deviceType_iPhone:
+          return getiPhoneModels();
+        case constants.deviceType_iPad:
+          return getiPadModels();
+        case constants.deviceType_iPod_touch:
+          return getiPodModels();
+        case constants.deviceType_Mac:
+          return getMacModels();
+        default:
+          return [];
+      }
+    },
   }
 }
 </script>
