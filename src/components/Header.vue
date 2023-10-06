60db1e614c37f1b3a0809bb55504e2e9128b61a4
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-06 16:56:01 -0400

    move device typer dropdown from Header to Support page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,22 +2,11 @@
   <div>
     <h1>Emoji Kitchen Keybaord</h1>
 
-    <v-select
-      label="Device type"
-      :items="deviceTypes"
-      variant="outlined"
-    ></v-select>
   </div>
 </template>
 
 <script>
-import getDeviceTypeList from '@/utils/getDeviceTypeList'
 export default {
   name: 'HeaderItem',
-  data() {
-    return {
-      deviceTypes: getDeviceTypeList()
-    }
-  }
 }
 </script> 
--- src/components/Support.vue
+++ src/components/Support.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     <h1>Support</h1>
+
+    <v-select
+      label="Device type"
+      :items="deviceTypes"
+      variant="outlined"
+    ></v-select>
   </div>
 </template>
 
 <script>
+import getDeviceTypeList from '@/utils/getDeviceTypeList'
 export default {
-  name: 'SupportPage'
+  name: 'SupportPage',
+  data() {
+    return {
+      deviceTypes: getDeviceTypeList()
+    }
+  }
 }
 </script>
