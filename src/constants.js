d5a3f92f7fab81ed5c7ff59e15d0242c0ab8b815
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-06 15:54:51 -0400

    add iPhone model names constants

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,38 @@
+// device type
 export const deviceType_iPhone = 'iPhone';
 export const deviceType_iPad = 'iPad';
+
+// device model
+export const iPhone_6s = 'iPhone 6s';
+export const iPhone_6sPlus = 'iPhone 6s Plus';
+export const iPhone_SE_1stGen = 'iPhone SE (1st generation)';
+export const iPhone_7 = 'iPhone 7';
+export const iPhone_7Plus = 'iPhone 7 Plus';
+export const iPhone_8 = 'iPhone 8';
+export const iPhone_8Plus = 'iPhone 8 Plus';
+export const iPhone_X = 'iPhone X';
+export const iPhone_XR = 'iPhone XR';
+export const iPhone_XS = 'iPhone XS';
+export const iPhone_XSMax = 'iPhone XS Max';
+export const iPhone_11 = 'iPhone 11';
+export const iPhone_11Pro = 'iPhone 11 Pro';
+export const iPhone_11ProMax = 'iPhone 11 Pro Max';
+export const iPhone_SE_2ndGen = 'iPhone SE (2nd generation)';
+export const iPhone_12Mini = 'iPhone 12 Mini';
+export const iPhone_12 = 'iPhone 12';
+export const iPhone_12Pro = 'iPhone 12 Pro';
+export const iPhone_12ProMax = 'iPhone 12 Pro Max';
+export const iPhone_13Mini = 'iPhone 13 Mini';
+export const iPhone_13 = 'iPhone 13';
+export const iPhone_13Pro = 'iPhone 13 Pro';
+export const iPhone_13ProMax = 'iPhone 13 Pro Max';
+export const iPhone_14 = 'iPhone 14';
+export const iPhone_14Plus = 'iPhone 14 Plus';
+export const iPhone_14Pro = 'iPhone 14 Pro';
+export const iPhone_14ProMax = 'iPhone 14 Pro Max';
+export const iPhone_15 = 'iPhone 15';
+export const iPhone_15Plus = 'iPhone 15 Plusi';
+export const iPhone_15Pro = 'iPhone 15 Pro';
+export const iPhone_15ProMax = 'iPhone 15 Pro Max';
+
+
