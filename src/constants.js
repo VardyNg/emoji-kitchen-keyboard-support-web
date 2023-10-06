4fb14a22542fc66c433a74c25920f1cd510ea741
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-06 17:04:13 -0400

    add iPhone, iPad, iPod Touch, and Mac model names constants.  add macOS, iOS/iPadOS name constants

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,9 @@ export const deviceType_iPhone = 'iPhone';
 export const deviceType_iPod_touch = 'iPod touch';
 export const deviceType_iPad = 'iPad';
 export const deviceType_Mac = 'Mac';
+
 // device model
+// -- iPhone
 export const iPhone_6s = 'iPhone 6s';
 export const iPhone_6sPlus = 'iPhone 6s Plus';
 export const iPhone_SE_1stGen = 'iPhone SE (1st generation)';
@@ -36,4 +38,122 @@ export const iPhone_15Plus = 'iPhone 15 Plusi';
 export const iPhone_15Pro = 'iPhone 15 Pro';
 export const iPhone_15ProMax = 'iPhone 15 Pro Max';
 
+// --- iPad
+export const iPad_8thGen = 'iPad 8th generation (2020)';
+export const iPad_9thGen = 'iPad 9th generation (2021)';
+export const iPad_10thGen = 'iPad 10th generation (2022)';
 
+export const iPad_mini_4thGen = 'iPad mini 4th generation (2015)';
+export const iPad_mini_5thGen = 'iPad mini 5th generation (2019)';
+export const iPad_mini_6thGen = 'iPad mini 6th generation (2021)';
+
+export const iPad_air_2thGen = 'iPad Air 2rd generation (2014)';
+export const iPad_air_3thGen = 'iPad Air 3rd generation (2019)';
+export const iPad_air_4thGen = 'iPad Air 4th generation (2020)';
+export const iPad_air_5thGen = 'iPad Air 5th generation (2022)';
+
+export const iPad_Pro_1stGen_9_7 = 'iPad Pro 1st generation (2015) (9.7-inch)';
+export const iPad_Pro_1stGen_12_9 = 'iPad Pro 1st generation (2016) (12.9-inch)';
+export const iPad_Pro_2ndGen_10_5 = 'iPad Pro 2nd generation (2017) (10.5-inch)';
+export const iPad_Pro_2ndGen_12_9 = 'iPad Pro 2nd generation (2017) (12.9-inch)';
+export const iPad_Pro_3rdGen_11 = 'iPad Pro 3rd generation (2018) (11-inch)';
+export const iPad_Pro_3rdGen_12_9 = 'iPad Pro 3rd generation (2018) (12.9-inch)';
+export const iPad_Pro_4thGen_11 = 'iPad Pro 4th generation (2020) (11-inch)';
+export const iPad_Pro_4thGen_12_9 = 'iPad Pro 4th generation (2020) (12.9-inch)';
+export const iPad_Pro_5thGen_11 = 'iPad Pro 5th generation (2021) (11-inch)';  
+export const iPad_Pro_5thGen_12_9 = 'iPad Pro 5th generation (2021) (12.9-inch)';
+export const iPad_Pro_6thGen_11 = 'iPad Pro 6th generation (2022) (11-inch)';
+export const iPad_Pro_6thGen_12_9 = 'iPad Pro 6th generation (2022) (12.9-inch)';
+
+// --- iPod Touch
+export const iPodTouch_7thGen = 'iPod Touch 7th generation (2019)';
+
+// --- Mac
+export const MacbookPro_2023 = 'Macbook Pro 2023 (14-inch/16-inch) (M2 Pro/M2 Max)';
+export const MacbookPro_2022 = 'Macbook Pro 2022 (13-inch) (M2)';
+export const MacbookPro_2021 = 'Macbook Pro 2023 (14-inch/16-inch) (M2 Pro/M2 Max)';
+export const MacbookPro_2020 = 'Macbook Pro 2020 (13-inch) (M1)';
+
+export const MacbookAir_2020 = "Macbook Air 2020 (13-inch) (M1)"
+export const MacbookAir_2022 = "Macbook Air 2022 (13-inch) (M2)"
+export const MacbookAir_2023 = "Macbook Air 2023 (15-inch) (M2)"
+
+export const MacMini_2020 = "Mac Mini 2020 (M1)"
+export const MacMini_2023 = "Mac Mini 2023 (M2/M2 Pro)"
+
+export const MacStudio_2022 = "Mac Studio 2022 (M1 Max / M1 Ultra)"
+export const MacStudio_2023 = "Mac Studio 2023 (M2 Max / M2 Ultra)"
+
+export const MacPro2023 = "Mac Pro 2023 (M2 Ultra)"
+
+// iOS
+export const iOS_14_0 = 'iOS/iPadOS 14.0';
+export const iOS_14_0_1 = 'iOS/iPadOS 14.0.1';
+export const iOS_14_1 = 'iOS/iPadOS 14.1';
+export const iOS_14_2 = 'iOS/iPadOS 14.2';
+export const iOS_14_2_1 = 'iOS/iPadOS 14.2.1';
+export const iOS_14_3 = 'iOS/iPadOS 14.3';
+export const iOS_14_4 = 'iOS/iPadOS 14.4';
+export const iOS_14_4_1 = 'iOS/iPadOS 14.4.1';
+export const iOS_14_4_2 = 'iOS/iPadOS 14.4.2';
+export const iOS_14_5 = 'iOS/iPadOS 14.5';
+export const iOS_14_5_1 = 'iOS/iPadOS 14.5.1';
+export const iOS_14_6 = 'iOS/iPadOS 14.6';
+export const iOS_14_7 = 'iOS/iPadOS 14.7';
+export const iOS_14_7_1 = 'iOS/iPadOS 14.7.1';
+export const iOS_14_8 = 'iOS/iPadOS 14.8';
+export const iOS_14_8_1 = 'iOS/iPadOS 14.8.1';
+
+export const iOS_15_0 = 'iOS/iPadOS 15.0';
+export const iOS_15_0_1 = 'iOS/iPadOS 15.0.1';
+export const iOS_15_0_2 = 'iOS/iPadOS 15.0.2';
+export const iOS_15_1 = 'iOS/iPadOS 15.1';
+export const iOS_15_1_1 = 'iOS/iPadOS 15.1.1';
+export const iOS_15_2 = 'iOS/iPadOS 15.2';
+export const iOS_15_2_1 = 'iOS/iPadOS 15.2.1';
+export const iOS_15_3 = 'iOS/iPadOS 15.3';
+export const iOS_15_3_1 = 'iOS/iPadOS 15.3.1';
+export const iOS_15_4 = 'iOS/iPadOS 15.4';
+export const iOS_15_4_1 = 'iOS/iPadOS 15.4.1';
+export const iOS_15_5 = 'iOS/iPadOS 15.5';
+export const iOS_15_6 = 'iOS/iPadOS 15.6';
+export const iOS_15_6_1 = 'iOS/iPadOS 15.6.1';
+export const iOS_15_7 = 'iOS/iPadOS 15.7';
+export const iOS_15_7_1 = 'iOS/iPadOS 15.7.1';
+export const iOS_15_7_2 = 'iOS/iPadOS 15.7.2';
+export const iOS_15_7_3 = 'iOS/iPadOS 15.7.3';
+export const iOS_15_7_4 = 'iOS/iPadOS 15.7.4';
+export const iOS_15_7_5 = 'iOS/iPadOS 15.7.5';
+export const iOS_15_7_6 = 'iOS/iPadOS 15.7.6';
+export const iOS_15_7_7 = 'iOS/iPadOS 15.7.7';
+export const iOS_15_7_8 = 'iOS/iPadOS 15.7.8';
+export const iOS_15_7_9 = 'iOS/iPadOS 15.7.9';
+
+export const iOS_16_0 = 'iOS/iPadOS 16.0';
+export const iOS_16_0_1 = 'iOS/iPadOS 16.0.1';
+export const iOS_16_0_2 = 'iOS/iPadOS 16.0.2';
+export const iOS_16_0_3 = 'iOS/iPadOS 16.0.3';
+export const iOS_16_1 = 'iOS/iPadOS 16.1';
+export const iOS_16_1_1 = 'iOS/iPadOS 16.1.1';
+export const iOS_16_1_2 = 'iOS/iPadOS 16.1.2';
+export const iOS_16_2 = 'iOS/iPadOS 16.2';
+export const iOS_16_3 = 'iOS/iPadOS 16.3';
+export const iOS_16_3_1 = 'iOS/iPadOS 16.3.1';
+export const iOS_16_4 = 'iOS/iPadOS 16.4';
+export const iOS_16_4_1 = 'iOS/iPadOS 16.4.1';
+export const iOS_16_5 = 'iOS/iPadOS 16.5';
+export const iOS_16_5_1 = 'iOS/iPadOS 16.5.1';
+export const iOS_16_6 = 'iOS/iPadOS 16.6';
+export const iOS_16_6_1 = 'iOS/iPadOS 16.6.1';
+export const iOS_16_7 = 'iOS/iPadOS 16.7';
+
+export const iOS_17_0 = 'iOS/iPadOS 17.0';
+export const iOS_17_0_1 = 'iOS/iPadOS 17.0.1';
+export const iOS_17_0_2 = 'iOS/iPadOS 17.0.2';
+export const iOS_17_0_3 = 'iOS/iPadOS 17.0.3';
+
+// macOS
+export const macOS_11 = 'macOS 11 Big Sur';
+export const macOS_12 = 'macOS 12 Monterey';
+export const macOS_13 = 'macOS 13 Ventura';
+export const macOS_14 = 'macOS 14 Sonoma';
