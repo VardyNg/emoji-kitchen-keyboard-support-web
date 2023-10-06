c579259909a09e04c2e0f3a1c0d846f177124e1d
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-06 15:49:03 -0400

    add select menu, display all available device type by calling getDeviceTypeList function

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
     <h1>Emoji Kitchen Keybaord</h1>
+
+    <v-select
+      label="Device type"
+      :items="deviceTypes"
+      variant="outlined"
+    ></v-select>
   </div>
 </template>
 
 <script>
+import getDeviceTypeList from '@/utils/getDeviceTypeList'
 export default {
-  name: 'HeaderItem'
+  name: 'HeaderItem',
+  data() {
+    return {
+      deviceTypes: getDeviceTypeList()
+    }
+  }
 }
-</script>
+</script> 
