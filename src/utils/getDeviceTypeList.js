cfd786347a99fe1a6c50e9f34c7c6a923d16acea
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-06 15:48:35 -0400

    add getDeviceTypeList function, return available device list form constants

---
+++ src/utils/getDeviceTypeList.js
@@ -0,0 +1,10 @@
+// deviceTypes.js
+
+import { deviceType_iPhone, deviceType_iPad } from '@/constants';
+
+export default function getDeviceTypeList() {
+  return [
+    deviceType_iPhone,
+    deviceType_iPad,
+  ];
+}
