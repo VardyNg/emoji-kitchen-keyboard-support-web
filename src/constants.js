2d0dcf502bd7dc4e7c29cb887021992b7c780dd8
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-06 17:03:37 -0400

    add iPodTouch to device type

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 // device type
 export const deviceType_iPhone = 'iPhone';
+export const deviceType_iPod_touch = 'iPod touch';
 export const deviceType_iPad = 'iPad';
 export const deviceType_Mac = 'Mac';
 // device model
