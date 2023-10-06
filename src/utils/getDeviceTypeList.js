40c990b1967140c19fc41c7201ba7dffa719b69d
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-06 16:40:44 -0400

    add deviceType_Mac export in getDeviceTypeList function

--- src/utils/getDeviceTypeList.js
+++ src/utils/getDeviceTypeList.js
@@ -1,10 +1,15 @@
 // deviceTypes.js
 
-import { deviceType_iPhone, deviceType_iPad } from '@/constants';
+import { 
+  deviceType_iPhone,
+  deviceType_iPad,
+  deviceType_Mac,
+} from '@/constants';
 
 export default function getDeviceTypeList() {
   return [
     deviceType_iPhone,
     deviceType_iPad,
+    deviceType_Mac,
   ];
 }
