2591434f4ccbb2266d4e092cccd0545872201a20
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-07 10:55:41 -0400

    add CopyRight page to display copy right information

---
+++ src/components/CopyRight.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>©2023 {{ author }}. All right reserved.</div>
+  
+</template>
+<script>
+import * as constants from '@/constants';
+export default {
+  name: 'CopyRight',
+  data() {
+    return {
+      author: constants.general.author
+    }
+  }
+}
+
+</script>
